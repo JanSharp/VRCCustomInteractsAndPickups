a8eee45f0dd4f129b36aee51a76196a0656cfff4
JanSharp <steinmeister36@gmail.com>
2024-07-22 12:23:11 +0200

    Clean up manager

--- Runtime/Scripts/CustomInteractsAndPickupsManager.cs
+++ Runtime/Scripts/CustomInteractsAndPickupsManager.cs
@@ -11,14 +11,21 @@ namespace JanSharp
     public class CustomInteractsAndPickupsManager : UdonSharpBehaviour
     {
         public Material highlightMat;
-        public LayerMask pickupLayer;
         public GameObject highlightPartPrefab;
         public Transform highlightTextRoot;
         public Transform highlightTextTransform;
         public TextMeshPro highlightText;
 
+        private LayerMask interactLayer = (LayerMask)(1 << 8);
+        private LayerMask pickupLayer = (LayerMask)(1 << 13);
+
         private VRCPlayerApi localPlayer;
+        private VRCPlayerApi.TrackingData head;
+        private Vector3 headPosition;
+        private Quaternion headRotation;
+        private Vector3 headForward;
         private CustomInteract interact;
+        private Transform interactTransform;
 
         private void Start()
         {
@@ -33,25 +40,74 @@ namespace JanSharp
                 interact = null;
                 highlightTextRoot.gameObject.SetActive(false);
             }
-            VRCPlayerApi.TrackingData head = localPlayer.GetTrackingData(VRCPlayerApi.TrackingDataType.Head);
-            Vector3 headPosition = head.position;
-            Quaternion headRotation = head.rotation;
-            Vector3 headForward = headRotation * Vector3.forward;
-            if (!Physics.Raycast(headPosition, headForward, out RaycastHit hit, 100f, pickupLayer, QueryTriggerInteraction.Collide))
-                return;
-            interact = hit.transform.GetComponentInParent<CustomInteract>();
-            if (interact == null)
-                return;
-            if (interact.interactText != "")
+            FetchHeadValues();
+            CustomInteract newInteract = TryGetInteract();
+            if (newInteract == interact)
             {
-                highlightText.text = interact.interactText;
-                highlightTextRoot.position = interact.transform.position;
-                highlightTextRoot.rotation = headRotation;
-                highlightTextTransform.localScale = Vector3.one * Vector3.Distance(headPosition, interact.transform.position);
-                highlightTextRoot.gameObject.SetActive(true);
+                UpdateHighlightText();
+                return;
             }
+            SetInteract(newInteract);
+        }
+
+        private void FetchHeadValues()
+        {
+            head = localPlayer.GetTrackingData(VRCPlayerApi.TrackingDataType.Head);
+            headPosition = head.position;
+            headRotation = head.rotation;
+            headForward = headRotation * Vector3.forward;
+        }
+
+        private CustomInteract TryGetInteract()
+        {
+            if (!Physics.Raycast(headPosition, headForward, out RaycastHit hit, 100f, interactLayer, QueryTriggerInteraction.Collide))
+                return null;
+            Transform hitTransform = hit.transform;
+            if (hitTransform == null) // Some VRC internal that we're not allowed to access so we get null instead,
+                return null; // even though in normal Unity if we have a hit... this is not possible to be null.
+            return hitTransform.GetComponentInParent<CustomInteract>();
+        }
+
+        private void SetInteract(CustomInteract newInteract)
+        {
+            if (newInteract == null)
+            {
+                if (interact == null)
+                    return;
+                interact.HideHighlight();
+                interact = null;
+                interactTransform = null;
+                HideHighlightText();
+                return;
+            }
+            interact = newInteract;
+            interactTransform = newInteract.transform;
             interact.manager = this;
             interact.ShowHighlight();
+            UpdateHighlightText();
+            ShowHighlightText();
+        }
+
+        private void ShowHighlightText()
+        {
+            highlightTextRoot.gameObject.SetActive(true);
+        }
+
+        private void HideHighlightText()
+        {
+            highlightTextRoot.gameObject.SetActive(false);
+        }
+
+        private void UpdateHighlightText()
+        {
+            if (interact == null)
+                return;
+            highlightText.text = interact.interactText;
+            // TODO: Maybe calculate the total bounds of all renderers and use the center of the bounds instead.
+            Vector3 interactPosition = interactTransform.position;
+            highlightTextRoot.position = interactPosition;
+            highlightTextRoot.rotation = headRotation;
+            highlightTextTransform.localScale = Vector3.one * Vector3.Distance(headPosition, interactPosition);
         }
 
         public override void InputUse(bool value, UdonInputEventArgs args)
