07f8290d9d3533ceb9a3c7f64723140716145e14
JanSharp <steinmeister36@gmail.com>
2025-02-13 16:38:17 +0100

    Add debug logging of inputs in dev scene

---
+++ Runtime/Dev/InputLogging.cs
@@ -0,0 +1,54 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using VRC.Udon.Common;
+
+namespace JanSharp
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class InputLogging : UdonSharpBehaviour
+    {
+        public GenericValueEditor valueEditor;
+        private ButtonWidgetData clearButtonData;
+        private GroupingWidgetData root;
+
+        private void Start()
+        {
+            root = valueEditor.NewGrouping();
+            clearButtonData = (ButtonWidgetData)valueEditor.NewButton("Clear")
+                .SetListener(this, nameof(Clear));
+            Clear();
+            Redraw();
+        }
+
+        private void Redraw()
+        {
+            valueEditor.Draw(new WidgetData[] { root });
+        }
+
+        public void Clear()
+        {
+            root.ClearChildren();
+            root.AddChild(clearButtonData);
+        }
+
+        public override void InputUse(bool value, UdonInputEventArgs args)
+        {
+            root.AddChild((LabelWidgetData)valueEditor.NewLabel($"InputUse, value: {value}, handType {(args.handType == HandType.RIGHT ? "right" : "left")}").StdMove());
+            Redraw();
+        }
+
+        public override void InputGrab(bool value, UdonInputEventArgs args)
+        {
+            root.AddChild((LabelWidgetData)valueEditor.NewLabel($"InputGrab, value: {value}, handType {(args.handType == HandType.RIGHT ? "right" : "left")}").StdMove());
+            Redraw();
+        }
+
+        public override void InputDrop(bool value, UdonInputEventArgs args)
+        {
+            root.AddChild((LabelWidgetData)valueEditor.NewLabel($"InputDrop, value: {value}, handType {(args.handType == HandType.RIGHT ? "right" : "left")}").StdMove());
+            Redraw();
+        }
+    }
+}
