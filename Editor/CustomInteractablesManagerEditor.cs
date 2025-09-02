550948938775222c3e10c5fbee970cf27c70f08a
JanSharp <steinmeister36@gmail.com>
2025-09-02 21:29:34 +0200

    Add custom inspector for manager to hide internals

---
+++ Editor/CustomInteractablesManagerEditor.cs
@@ -0,0 +1,36 @@
+using JanSharp.Internal;
+using UdonSharpEditor;
+using UnityEditor;
+#if CUSTOM_INTERACTS_AND_PICKUPS_DEBUG
+using UnityEngine;
+#endif
+
+namespace JanSharp
+{
+    [CanEditMultipleObjects]
+    [CustomEditor(typeof(CustomInteractablesManager))]
+    public class CustomInteractablesManagerEditor : Editor
+    {
+        private SerializedObject so;
+        private SerializedProperty autoHoldModeProp;
+
+        public void OnEnable()
+        {
+            so = serializedObject;
+            autoHoldModeProp = so.FindProperty("autoHoldMode");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            if (UdonSharpGUI.DrawDefaultUdonSharpBehaviourHeader(targets))
+                return;
+            so.Update();
+            EditorGUILayout.PropertyField(autoHoldModeProp);
+#if CUSTOM_INTERACTS_AND_PICKUPS_DEBUG
+            GUILayout.Label("Debug / Internal", EditorStyles.boldLabel);
+            DrawPropertiesExcluding(so, "m_Script", "autoHoldMode");
+#endif
+            so.ApplyModifiedProperties();
+        }
+    }
+}
